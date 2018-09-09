cb60c91ad1a61d52f3b1cb61942946af7db0f099
Sushant <sushant.kumar@pharmeasy.in>
2018-09-09 19:52:01 +0530

    tutor save and list api

---
+++ src/main/java/com/hansa/app/TutorRepo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hansa.app;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author sushant
+ */
+
+public interface TutorRepo extends CrudRepository<Tutor, Long> {
+    
+    
+}
